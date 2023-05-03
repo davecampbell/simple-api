510a7a9f3b0db6e27fc75b617972c9ac3f7d739a
Dave Campbell <daveccampbell@gmail.com>
2023-05-03 19:05:48 +0000

    respond to different queue name patterns

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import datetime
+import random
+
 app = FastAPI()
 
 
@@ -25,9 +28,19 @@ def read_queue(q_name: str):
 #        "inflightMessages": 0
 #    }
 
-    messages = 7
-    inflightMessages = 0
+    now = datetime.datetime.now()
+
+    if "_hr" in q_name:
+        messages = now.minute + int(now.second/15)
+        inflightMessages = random.randint(0,int(now.second/15))
+
+    elif "_15" in q_name:
+        messages = now.minute%15
+        inflightMessages = random.randint(0,messages%4)
+
+    else:
+        messages = now.hour * 60 + now.minute
+        inflightMessages = random.randint(0,20)
 
     return {"destination": q_name, "messages": messages, "inflightmessages": inflightMessages}
-
 
