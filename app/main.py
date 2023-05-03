c58117c29dd14dc365d23739fc7237d1549f72f7
Dave Campbell <dave@daves-air.lan>
2023-05-03 08:40:31 -0400

    add a mulesoft endpoint

--- app/main.py
+++ app/main.py
@@ -14,3 +14,20 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/mock/mulesoft/queue/{q_name}")
+def read_queue(q_name: str):
+# go find some values
+# result payload should look like this
+#
+# {
+#        "destination": "Queue1",
+#        "messages": 7,
+#        "inflightMessages": 0
+#    }
+
+    messages = 7
+    inflightMessages = 0
+
+    return {"destination": q_name, "messages": messages, "inflightmessages": inflightMessages}
+
+
